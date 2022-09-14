f5c0ed535d2b7e45fba3b8acac92e063eef5f370
BNY\eymen <bunyamineymenalagoz@gmail.com>
2022-09-14 10:32:39 +0300

    phase_16

--- Assets/Demo_5/Demo5Script1.cs
+++ Assets/Demo_5/Demo5Script1.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Demo5Script1 : MonoBehaviour
@@ -9,5 +10,9 @@ public class Demo5Script1 : MonoBehaviour
         Debug.Log("Start 1");
     }
 
+    public void MainLogic()
+    {
+        Debug.Log("MainLogic");
+    }
 
 }
